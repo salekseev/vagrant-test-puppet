a43e398313cfb7ce08e9bf206932826b42bd6415
Stas Alekseev <stas.alekseev@gmail.com>
2014-07-16 16:53:16 -0400

    Adding build user.

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ quick start:
 ```
 # install vagrant-puppet-install for vagrant
 $ vagrant plugin install vagrant-puppet-install
+# optionally also install to speed things up
+$ vagrant plugin install vagrant-cachier
 $ git clone this repo
 $ vagrant up
 ```
--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,21 @@
 node 'puppet.vagrant.local' {
+  user { 'build':
+    ensure     => present,
+    uid        => '1001',
+    gid        => 'ath',
+    comment    => 'Build',
+    home       => '/home/build',
+    shell      => '/bin/bash',
+    managehome => true,
+    require    => Group['ath'],
+  }
+
+  group { 'ath':
+    ensure => 'present',
+    name   => 'ath',
+    gid    => '1003',
+  }
+
   package { 'p4':
     ensure   => 'installed',
     source   => 'http://cobbler200.athenahealth.com/LocalRepo/athena-6/RPMS/p4-2014.1.807760-1.x86_64.rpm',
